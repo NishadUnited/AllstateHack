dae62c03c9d783e3b1994a9582cea9e62aa4ac56
Pranay Prakash <pranay.gp@gmail.com>
2015-10-10 00:56:19 -0500

    Made script that acts as webhook and pushes data to Parse

--- src/main/java/com/example/data/CallbackController.java
+++ src/main/java/com/example/data/CallbackController.java
@@ -8,8 +8,13 @@ import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestOperations;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Api(value = "Callback", description = "Callback API")
@@ -17,10 +22,9 @@ public class CallbackController {
     private static final Logger logger = LoggerFactory.getLogger(CallbackController.class);
 
     @Autowired
-    private SubscriberProperties subscriberProperties;
+    private RestOperations restOperations;
 
-    @Autowired
-    private DataRepository dataRepository;
+    private String facebookBaseUrl = "https://api.parse.com/1/classes/sensorData";
 
     @ApiOperation(
             value = "Receive A6 data",
@@ -38,9 +42,10 @@ public class CallbackController {
                          String signature) {
         logger.debug("/callback: body = [" + body + "]");
 
-        DataEntity data = new DataEntity();
-        data.setSignature(signature);
-        data.setData(((DBObject) JSON.parse(body)));
-        dataRepository.insert(data);
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        headers.add("X-Parse-Application-Id", "RlOF7ViCpvt0d7H8nE7TYHz8HbQtSCfma3GopAsg"); //TODO: Update ID
+        headers.add("X-Parse-REST-API-Key", "kCxkFLu5TwgxCYrtwC2vB6Ru5NZOhR1NROJZCPxq"); //TODO: Update ID
+        restOperations.exchange(facebookBaseUrl, HttpMethod.POST, new HttpEntity<>(body, headers), Map.class);
+//        restOperations.postForObject(facebookBaseUrl, body, null);
     }
 }
